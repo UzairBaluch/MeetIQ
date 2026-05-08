df3d6dc1abc41910966c7dc15d060259a8b8b151
Uzair Baluch <uzairbaluchdev@gmail.com>
2026-05-08 12:27:29 +0400

    Add ApiResponse class for standardized API responses
    Introduce a new ApiResponse class in apiResponse.ts to facilitate consistent handling of API responses. This class includes properties for status code, data, message, and success status, enhancing the clarity and structure of responses across the backend.

---
+++ backend/src/utils/apiResponse.ts
@@ -0,0 +1,20 @@
+
+class ApiResponse {
+
+    statusCode: number;
+
+    data: unknown;
+  
+    message: string;
+  
+    success: boolean;
+  
+    constructor(statusCode: number, data: unknown, message: string = 'success') {
+      this.statusCode = statusCode;
+      this.data = data;
+      this.message = message;
+      this.success = statusCode < 400;
+    }
+  }
+  
+  export { ApiResponse };
